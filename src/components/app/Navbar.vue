81328b3d768df2084df14bc71ec4c2b46387bc39
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-01-20 21:26:56 +0300

    navbar and sidebar components

---
+++ src/components/app/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav class="navbar blue darken-4">
+    <div class="nav-wrapper">
+      <div class="navbar-left">
+        <a href="#" @click.prevent="$emit('click')">
+          <i class="material-icons">menu</i>
+        </a>
+        <div class="navbar-header" @click.prevent="$emit('click')">Финансы</div>
+
+      </div>
+
+      <ul class="right">
+        <li>
+          <a
+            class="dropdown-trigger"
+            href="#"
+            data-target="dropdown"
+          >
+            USER NAME
+            <i class="material-icons right">arrow_drop_down</i>
+          </a>
+
+          <ul id='dropdown' class='dropdown-content'>
+            <li>
+              <a href="#" class="black-text">
+                <i class="material-icons">account_circle</i>Профиль
+              </a>
+            </li>
+            <li class="divider" tabindex="-1"></li>
+            <li>
+              <a href="#" class="black-text">
+                <i class="material-icons">assignment_return</i>Выйти
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+name: "Navbar"
+}
+</script>
+
+<style scoped>
+.navbar-header {
+  font-size: 2rem;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul
+    class="sidenav app-sidenav"
+    :class="{'open': value}"
+  >
+
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      tag="li"
+      active-class="active"
+      :to="link.url"
+      :exact="link.exact"
+    >
+      <a
+        href="#"
+        class="waves-effect waves-orange pointer"
+      >
+        {{ link.title }}
+      </a>
+    </router-link>
+
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  props: ['value'],
+  data: () => ({
+    links: [
+      {title: 'Счет', url: '/', exact: true},
+      {title: 'История', url: '/history', exact: false},
+      {title: 'Планирование', url: '/planning', exact: false},
+      {title: 'Новая запись', url: '/record', exact: false},
+      {title: 'Категория', url: '/categories', exact: false}
+    ]
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
