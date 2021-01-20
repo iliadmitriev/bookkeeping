b400633056bb9ed2705e8886d7c4ca577467a336
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-01-20 20:08:35 +0300

    added 2 layouts and 9 pages: * layouts (main, empty) * pages (login, register, home, categories, detail-record, planning, history, profile, record * setup all routes and css styles

--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,58 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: () => import('@/views/Login')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import('@/views/Register')
+  },
+  {
     path: '/',
     name: 'Home',
+    meta: {layout: 'main'},
     component: () => import('@/views/Home')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: () => import('@/views/Categories')
+  },
+  {
+    path: '/detail-record',
+    name: 'detail-record',
+    meta: {layout: 'main'},
+    component: () => import('@/views/DetailRecord')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import('@/views/Planning')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('@/views/History')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('@/views/Profile')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('@/views/Record')
   }
 ]
 
