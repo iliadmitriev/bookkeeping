652ac789b565d3105af55b84e634d58f66a3eee9
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-01-21 09:35:23 +0300

    too many requests

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -3,5 +3,6 @@ export default {
   'auth/user-not-found': 'Пользователь не найден',
   'auth/wrong-password': 'Указан не верный пароль',
   'auth/user-disabled': 'Пользователю запрещен доступ в систему',
-  'auth/email-already-in-use': 'Пользователь с таким email уже есть в системе'
+  'auth/email-already-in-use': 'Пользователь с таким email уже есть в системе',
+  'auth/too-many-requests': 'Слишком много попыток, подождите немного'
 }
