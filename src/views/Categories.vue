b056bac43ba9e15e6c6bf10e3b5be610c59cc0a8
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-01-21 18:14:22 +0300

    * load categories from db * categories edit * categories cache

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -6,9 +6,23 @@
     <section>
       <div class="row">
 
-        <CategoryCreate @created="addNewCategory" />
+        <CategoryCreate @created="addNewCategory"/>
 
-        <CategoryEdit />
+        <loader
+          v-if="loading"
+        />
+        <div
+          class="row"
+          v-else
+        >
+          <CategoryEdit
+            v-if="categories.length"
+            :categories="categories"
+            :key="categories.length + updateCount"
+            @updated="updateCategories"
+          />
+          <p v-else class="center">Категорий пока нет</p>
+        </div>
 
       </div>
     </section>
@@ -18,19 +32,37 @@
 <script>
 import CategoryCreate from "@/components/CategoryCreate";
 import CategoryEdit from "@/components/CategoryEdit";
+import Loader from "@/components/app/Loader";
 
 export default {
   name: "Categories",
-  data: ()=>({
-    categories: []
+  data: () => ({
+    categories: [],
+    loading: true,
+    updateCount: 0
   }),
+  async mounted() {
+    try {
+      this.categories = await this.$store.dispatch('fetchCategories')
+    } catch (e) {
+
+    } finally {
+      this.loading = false
+    }
+  },
   methods: {
     addNewCategory(cat) {
       this.categories.push(cat)
       console.log(this.categories)
+    },
+    updateCategories(category) {
+      const idx = this.categories.findIndex(c => c.id === category.id)
+      this.categories[idx].title = category.title
+      this.categories[idx].limit = category.limit
+      this.updateCount++
     }
   },
-  components: {CategoryEdit, CategoryCreate}
+  components: {Loader, CategoryEdit, CategoryCreate}
 }
 </script>
 
