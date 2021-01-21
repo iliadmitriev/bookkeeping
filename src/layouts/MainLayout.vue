4d2d02a00ae245a92ab184ce45c54e8598636ee9
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-01-21 14:40:05 +0300

    MainLayout.vue loader

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,40 +1,46 @@
 <template>
-  <div class="app-main-layout">
+  <div>
+    <loader v-if="loading" />
+    <div v-else class="app-main-layout">
 
-    <Navbar
-      @click="isOpen = !isOpen"
-    >
-    </Navbar>
+      <Navbar
+        @click="isOpen = !isOpen"
+      >
+      </Navbar>
 
-    <Sidebar v-model="isOpen"></Sidebar>
+      <Sidebar v-model="isOpen"></Sidebar>
 
-    <main class="app-content" :class="{'full': !isOpen}">
-      <div class="app-page">
-        <router-view/>
-      </div>
-    </main>
+      <main class="app-content" :class="{'full': !isOpen}">
+        <div class="app-page">
+          <router-view/>
+        </div>
+      </main>
 
-    <FloatButton></FloatButton>
+      <FloatButton></FloatButton>
+    </div>
   </div>
-
 </template>
 
 <script>
 import Navbar from "@/components/app/Navbar";
 import Sidebar from "@/components/app/Sidebar";
 import FloatButton from "@/components/app/FloatButton";
+import Loader from "@/components/app/Loader";
 
 export default {
   name: 'MainLayout',
   data: () => ({
-    isOpen: false
+    isOpen: false,
+    loading: true
   }),
   async mounted() {
     if (!Object.keys(this.$store.getters.info).length) {
       await this.$store.dispatch('fetchInfo')
     }
+    this.loading = false
   },
   components: {
+    Loader,
     Navbar,
     Sidebar,
     FloatButton
