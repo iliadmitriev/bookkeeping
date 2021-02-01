b7cf8b46d15f1bcd9101fb4e81e8e57a20e6fd98
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-01 21:58:55 +0300

    new categories design

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,31 +1,121 @@
 <template>
   <div>
-    <div class="page-title">
-      <h3>{{ 'Categories' | localize }}</h3>
-    </div>
-    <section>
-      <div class="row">
+    <h3>{{ 'Categories' | localize }}</h3>
 
-        <CategoryCreate @created="addNewCategory"/>
+    <v-data-table
+      :headers="categoryHeaders"
+      :items="categories"
+      class="elevation-1"
+    >
 
-        <loader
-          v-if="loading"
-        />
-        <div
-          class="row"
-          v-else
+
+      <template v-slot:top>
+        <v-toolbar
+          flat
         >
-          <CategoryEdit
-            v-if="categories.length"
-            :categories="categories"
-            :key="categories.length + updateCount"
-            @updated="updateCategories"
-          />
-          <p v-else class="center">{{ 'NoCategories' | localize }}</p>
-        </div>
+          <v-spacer></v-spacer>
+          <v-dialog
+            v-model="dialog"
+            max-width="500px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="primary"
+                dark
+                class="mb-2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                {{ 'Create' | localize }}
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">{{ formTitle }}</span>
+              </v-card-title>
 
-      </div>
-    </section>
+              <v-form
+                v-model="valid"
+                ref="editForm"
+                @submit.prevent="save"
+              >
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.title"
+                        :rules="titleRules"
+                        :label="'Title' | localize"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.limit"
+                        :rules="limitRules"
+                        :label="'Limit' | localize"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="close"
+                >
+                  {{ 'Cancel' | localize }}
+                </v-btn>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="save"
+                >
+                  {{ 'Save' | localize }}
+                </v-btn>
+              </v-card-actions>
+              </v-form>
+            </v-card>
+          </v-dialog>
+
+        </v-toolbar>
+      </template>
+
+
+      <template v-slot:item.limit="{ item }">
+        {{ item.limit | number }}
+      </template>
+
+      <template v-slot:item.actions="{ item }">
+        <v-icon
+          small
+          class="mr-2"
+          @click="editItem(item)"
+        >
+          mdi-pencil
+        </v-icon>
+      </template>
+
+    </v-data-table>
+
   </div>
 </template>
 
@@ -33,19 +123,49 @@
 import CategoryCreate from "@/components/CategoryCreate";
 import CategoryEdit from "@/components/CategoryEdit";
 import Loader from "@/components/app/Loader";
-import localizeFilter from "@/filters/localize.filter";
+import localize from "@/filters/localize.filter";
 
 export default {
   name: "Categories",
   metaInfo() {
     return {
       title: this.$title('Categories')
-    }},
+    }
+  },
   data: () => ({
+    dialog: false,
+    valid: false,
     categories: [],
+    categoryHeaders: [
+      {text: localize('Title'), value: 'title'},
+      {text: localize('Limit'), value: 'limit'},
+      {text: localize('Actions'), value: 'actions', sortable: false}
+    ],
+    titleRules: [
+      v => !!v || localize('EnterTitle')
+    ],
+    limitRules: [
+      v => !!v || localize('Required'),
+      v => v >= 100 || localize('MinValue') + ' 100'
+    ],
+    editedItem: {
+      id: null,
+      title: '',
+      limit: 100
+    },
+    defaultItem: {
+      id: null,
+      title: '',
+      limit: 100
+    },
     loading: true,
     updateCount: 0
   }),
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? localize('Create') : localize('Edit')
+    }
+  },
   async mounted() {
     try {
       this.categories = await this.$store.dispatch('fetchCategories')
@@ -56,15 +176,53 @@ export default {
     }
   },
   methods: {
-    addNewCategory(cat) {
-      this.categories.push(cat)
+
+    async createCategory(newCategory) {
+      try {
+        const category = await this.$store.dispatch('createCategory', newCategory)
+        this.$message(localize('CategoryCreated'))
+        return category
+      } catch (e) {}
     },
-    updateCategories(category) {
-      const idx = this.categories.findIndex(c => c.id === category.id)
-      this.categories[idx].title = category.title
-      this.categories[idx].limit = category.limit
-      this.updateCount++
-    }
+
+    async updateCategory(categoryData) {
+      try {
+        const category = await this.$store.dispatch('updateCategory', categoryData)
+        this.$message(localize('CategoryUpdated'))
+        return category
+      } catch (e) {}
+    },
+
+    editItem(item) {
+      this.editedIndex = this.categories.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    close() {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+      })
+    },
+
+    async save() {
+      if (!this.valid) {
+        this.$refs.editForm.validate()
+        return
+      }
+
+      debugger
+
+      if (!!this.editedItem.id) {
+        await this.updateCategory(this.editedItem)
+        Object.assign(this.categories[this.editedIndex], this.editedItem)
+      } else {
+        const newCategory = await this.createCategory(this.editedItem)
+        this.categories.push(newCategory)
+      }
+      this.close()
+    },
   },
   components: {Loader, CategoryEdit, CategoryCreate}
 }
