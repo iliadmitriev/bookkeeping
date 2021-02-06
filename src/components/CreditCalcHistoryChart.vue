6a4bf4cf3bd47689b5dcb5f33f9bd0804acdc9ed
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-06 13:18:08 +0300

    * debtTimelineOptions tooltip

--- src/components/CreditCalcHistoryChart.vue
+++ src/components/CreditCalcHistoryChart.vue
@@ -69,6 +69,11 @@ export default {
           xAxes: [{
             type: 'time',
             ticks: {},
+            time: {
+              displayFormats: {
+                quarter: 'MMM YYYY'
+              }
+            }
           }]
         },
         tooltips: {
