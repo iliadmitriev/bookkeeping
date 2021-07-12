3fc4fad08080cec8cb5b4880c60d35d28fe7b4e8
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-07-12 08:12:48 +0300

    raise threshold

--- jest.config.js
+++ jest.config.js
@@ -37,10 +37,10 @@ module.exports = {
   coveragePathIgnorePatterns: [],
   coverageThreshold: {
     global: {
-      branches: 30,
-      functions: 41,
-      lines: 50,
-      statements: 50
+      branches: 39,
+      functions: 46,
+      lines: 55,
+      statements: 55
     }
   },
 
